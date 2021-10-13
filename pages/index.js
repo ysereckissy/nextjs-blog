1ab4ce03e995e28b2326231ab67685ce4335b090
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2021-10-13 19:48:12 +0200

    add dymanic routes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 
@@ -32,11 +34,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+                <Link href={`/posts/${id}`}>
+                    <a>{title}</a>
+                </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
